f15d1434e23e0d645199d39538867d7e98f696aa
Guy Buys <guy.buys@tsmmechelen.be>
2025-10-13 11:00:48 +0000

    Update mqtt_config_example.py en README.md

--- mqtt_config_example.py
+++ mqtt_config_example.py
@@ -1,3 +1,5 @@
 # mqtt_config_example.py
 # Vul hier het IP-adres of de hostname van de MQTT broker in
 BROKER_ADDRESS = "127.0.0.1"  # voorbeeldadres, pas aan indien nodig
+SUB_TOPIC = "TEST/SUB"  # Hardcoded subscribe topic
+DEFAULT_PUB_TOPIC = "TEST/PUB"  # Default publish topic
